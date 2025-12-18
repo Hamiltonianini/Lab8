3bdf89e1a7e676cf918b84bb7047f5b75baad18d
Hamiltonianini <nuhednoi555@gmail.com>
2025-12-18 23:45:54 +0700

    Lab 8 completed!

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -1,16 +1,35 @@
-char findGrade(double grade){
-    char letter;
-    if(grade>90){
-        letter = 'A';
-    }else if(grade<=90 && grade>75){
-        letter = 'B';
-    }else if(grade<=75 && grade>60){
-        letter = 'C';
-    }else if(grade<=60 && grade>45){
-        letter = 'D';
-    }else{
-        letter = 'F';
-    }
-    
-    return letter;
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  char rank;
+  cout << "Input your rank: ";
+  cin >> rank;
+  if(rank=='S'){
+	cout << "You have received Super Ultra Rare Unit!!!\n";
+	cout << "You have received 5 gems.\n";
+	cout << "You have received 1 gems.\n";
+	cout << "You have received 2000 coins.\n";
+	cout << "You have received very KAK items. \n";
+
+ }else if(rank=='A'){
+	cout << "You have received 5 gems.\n";
+	cout << "You have received 1 gems.\n";
+	cout << "You have received 2000 coins.\n";
+	cout << "You have received very KAK items. \n";
+
+ }else if(rank=='B'){
+	cout << "You have received 1 gems.\n";
+	cout << "You have received 2000 coins.\n";
+	cout << "You have received very KAK items. \n";
+
+ }else if(rank=='C'){
+	cout << "You have received 2000 coins.\n";
+	cout << "You have received very KAK items. \n";
+
+ }else if(rank=='D'){
+	cout << "You have received very KAK items. \n";
+ }
+  return 0;
 }
